99f44eeb611f821e27b4c08a3df51a6a0c54d627
carlosjuanco <carlosjuancho328@gmail.com>
2024-06-13 00:08:42 -0600

    Instalar axios

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
+import axios from 'axios'
 import 'bulma/css/bulma.css'
+
+const api_token: string | null = localStorage.getItem('api_token')
+
+axios.defaults.baseURL = 'http://localhost:8080/api'
+axios.defaults.withCredentials = true
+
+if (api_token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${api_token}`
+}
+
 
 createApp(App).use(router).use(store).mount('#app')
