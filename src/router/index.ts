ea757ca0e1a879592e1aff50519f0b56ebb28269
carlosjuanco <carlosjuancho328@gmail.com>
2024-06-14 15:35:38 -0600

    Autorizacion a componentes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
+import store from '../store/index'
 
 import HelloWord from "../components/HelloWorld.vue"
 
@@ -13,6 +14,26 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.VUE_APP_BASE_URL),
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+    try {
+        await store.dispatch('check')
+
+        const page = store.getters.pages.find((page: {name: string}) => page.name === to.name)
+        if(page.name == to.name){
+            next()
+        }
+    }
+    catch (error) {
+        store.dispatch('destroySession')
+        if (to.name == 'login') {
+            next()
+        }
+        else {
+            next({ name: 'login' })
+        }
+    }
 })
 
 export default router
