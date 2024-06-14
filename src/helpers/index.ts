5673199561ae5f6e178ad37327653deee61082d9
carlosjuanco <carlosjuancho328@gmail.com>
2024-06-14 15:19:40 -0600

    Funciones de ayuda

---
+++ src/helpers/index.ts
@@ -0,0 +1,79 @@
+import axios from 'axios'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+const helpers = () => {
+    const store = useStore()
+    const router = useRouter()
+
+    const handleErrors = (error: any) => {
+        const errors:{ [key: string]: any } = {}
+        if (error.response) {
+            if (error.response.data.errors) {
+                Object.entries(error.response.data.errors).forEach(([key, value]) => {
+                    errors[key] = value
+                })
+            }
+            else {
+                errors['message'] = error.response.data.message
+            }
+        }
+        else {
+           errors['message'] = error.message
+        }
+
+        return errors
+    }
+
+    const logout = async () => {
+        try {
+            await store.dispatch('logout')
+
+            router.replace({ name: 'login' })
+        }
+        catch (error) {
+            // handleErrors(error)
+        }
+    }
+
+    const getInformationUser = () => {
+        return store.getters.user
+    }
+
+    interface Fields {
+        [key: string]: any // o un tipo más específico si sabes qué tipo de datos esperar
+    }
+
+    interface Data {
+        [key: string]: any // o un tipo más específico si sabes qué tipo de datos esperar
+    }
+
+    interface Form {
+        errors: { [key: string]: string }
+        data: { [key: string]: any }
+    }
+
+    const setForm = (fields: Fields, data: Data) => {
+        const form: Form = {
+            errors: {},
+            data: {}
+        }
+
+        Object.keys(fields).map(k => {
+            if (k != '_method') {
+                form.data[k] = data[k] ? data[k] : ''
+            }
+        })
+
+        return form
+    }
+
+    return {
+        handleErrors,
+        logout,
+        getInformationUser,
+        setForm
+    }
+}
+
+export default helpers
