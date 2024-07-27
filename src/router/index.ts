54ea72525c1e3ae95d4fee3536fc0eb2a5f217b3
carlosjuanco <carlosjuancho328@gmail.com>
2024-07-26 19:25:13 -0600

    vue-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router"
-import store from '../store/index'
+import store from '@/store/index'
 
-import HelloWord from "../components/HelloWorld.vue"
-import AppLogin from "../views/AppLogin.vue"
-import AppHome from "../views/AppHome.vue"
+import HelloWord from "@/components/HelloWorld.vue"
+import AppLogin from "@/views/AppLogin.vue"
+import AppHome from "@/views/AppHome.vue"
 
 const routes = [
     {
