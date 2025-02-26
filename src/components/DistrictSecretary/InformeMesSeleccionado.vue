e42aaa9e0e14940403d5fe2a38dff769a3a0d0d3
carlosjuanco <carlosjuancho328@gmail.com>
2025-02-25 20:47:53 -0600

    SDA-18 Hacer la maqueta del componente informe por meses

---
+++ src/components/DistrictSecretary/InformeMesSeleccionado.vue
@@ -0,0 +1,95 @@
+<script lang="ts">
+
+export default {
+  name: "InformeMesSeleccionado",
+  setup() {
+    const concepts: string[] = [
+        "Iglesias",
+        "Grupos pequeños",
+        "Contactos misioneros",
+        "Estudios acumulados",
+        "Nuevos estudios",
+        "Bautismos",
+        "Total de personas estudiando",
+        "Total de estudios mensuales",
+        "Total de bautismos alcanzados",
+        "Invitados en la campaña de GP",
+        "Invitados en la campaña de iglesia",
+    ]
+    const churches: string[] = [
+        "Centenario",
+        "Familias en Crecimiento",
+        "Las Flores",
+        "Volcanes",
+        "Donaji",
+        "Jardín",
+        "7 Regiones",
+        "San Luis Beltran",
+        "Zogocho",
+        "Huayapam",
+        "Yatareni",
+        "Tres Cruces",
+    ]
+
+    return {
+      concepts,
+      churches,
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="table-container">
+    <p class="title is-1 has-text-centered mt-1">Informe de las iglesias del distrito de las flores </p>
+    <p class="subtitle is-2 has-text-centered mt-1">Mes de Enero</p>
+    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+      <thead>
+        <tr class="is-light">
+          <th v-for="(concept, index) in concepts" :key="index" v-text="concept"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(church, index) in churches" :key="index">
+          <td v-text="church"></td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
+        <tr class="is-light">
+          <td>Anterior</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+        </tr>
+        <tr class="is-light">
+          <td>Total distrital</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
---
+++ src/components/DistrictSecretary/InformePorMeses.vue
@@ -0,0 +1,74 @@
+<script lang="ts">
+import {useRouter} from "vue-router"
+
+export default {
+  name: 'InformePorMeses',
+  setup() {
+    const router = useRouter()
+      const open_informeMesX = (mes: string) => {
+        console.log('raquela', mes)
+        router.replace({ name: 'Informe mes seleccionado' })
+      }
+
+      return {
+        open_informeMesX
+      }
+  }
+}
+</script>
+
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="select is-rounded is-large is-fullwidth">
+        <select>
+          <option>2024</option>
+          <option>2025</option>
+          <option>2026</option>
+        </select>
+      </div>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth" @click="open_informeMesX('Enero')">Enero</button>
+    </div>
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Febrero</button>
+    </div>
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Marzo</button>
+    </div>
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Abril</button>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Mayo</button>
+    </div>
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Junio</button>
+    </div>
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Julio</button>
+    </div>
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Agosto</button>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Septiembre</button>
+    </div>
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Octubre</button>
+    </div>
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Noviembre</button>
+    </div>
+    <div class="column">
+      <button class="button is-large pt-6 pb-6 is-fullwidth">Diciembre</button>
+    </div>
+  </div>
+</template>
