4eafd678ba6a71295b06a13f6144f4a67b311edd
carlosjuanco <carlosjuancho328@gmail.com>
2025-02-27 02:11:10 -0600

    SDA-27 Reglas de negocio del componente informe mensual actual

--- src/components/DistrictSecretary/InformeMensualActual.vue
+++ src/components/DistrictSecretary/InformeMensualActual.vue
@@ -91,4 +91,64 @@ export default {
     </tbody>
   </table>
 </div>
+  <div class="columns">
+      <div class="column">
+          <article class="panel is-link">
+              <p class="panel-heading">Reglas de negocio</p>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                La interseccion de iglesia y conceptos, es lo que capturo cada <span class="tag is-light">secretaria de iglesia</span>.
+              </a>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                La fila con el etiqueta <span class="tag is-light">anterior</span> es el total por cada columna del mes anterior.
+              </a>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                La fila con el etiqueta <span class="tag is-light">Total distrital</span> es el total del mes actual.
+              </a>
+          </article>
+      </div>
+      <div class="column">
+          <article class="panel is-link">
+              <p class="panel-heading">Funcionalidad</p>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                Los conceptos se traeran de la tabla <span class="tag is-light">conceptos</span>.
+              </a>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                Los nombres de iglesias se traeran de la tabla <span class="tag is-light">iglesias</span>.
+              </a>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                Obtener la información de la interseccion de iglesias y conceptos.
+              </a>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                El primero mes, cuando comience a usarse el sistema, todo estará en ceros.
+              </a>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                Cuando se cierre el primer mes, todo estará en ceros, menos la fila de <span class="tag is-light">Anterior</span>
+              </a>
+          </article>
+      </div>
+    </div>
 </template>
