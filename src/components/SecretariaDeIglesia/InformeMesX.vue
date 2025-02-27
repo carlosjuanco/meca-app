1e53f15aa199c6c72d1bb3dac9f2670c7049f6f2
carlosjuanco <carlosjuancho328@gmail.com>
2025-02-27 01:41:01 -0600

    SDA-26 Reglas de negocio del componente informe mes x

--- src/components/SecretariaDeIglesia/InformeMesX.vue
+++ src/components/SecretariaDeIglesia/InformeMesX.vue
@@ -1,28 +1,20 @@
 <script lang="ts">
-    import { Field, Form, ErrorMessage, FieldArray } from 'vee-validate'
-    import * as yup from 'yup'
 
 	export default {
         name: 'InformeMesX',
-        components: {
-            Form,
-            Field,
-            ErrorMessage,
-            FieldArray
-        },
         setup() {
             const concepts = [
             "",
-            "GRUPOS PEQUEÑOS",
-            "CONTACTOS MISIONEROS",
-            "ESTUDIOS ACUMULADOS",
-            "NUEVOS ESTUDIOS",
-            "BAUTISMOS",
-            "TOTAL DE PERSONAS ESTUDIANDO",
-            "TOTAL DE ESTUDIOS MENSUALES",
-            "TOTAL DE BAUTISMOS ALCANZADOS",
-            "INVITADOS EN LA CAMPAÑA DE GP",
-            "INVITADOS EN LA CAMPAÑA DE IGLESIA",
+            "Grupos pequeños",
+            "Contactos misioneros",
+            "Estudios acumulados",
+            "Nuevos estudios",
+            "Bautismos",
+            "Total de personas estudiando",
+            "Total de estudios mensuales",
+            "Total de bautismos alcanzados",
+            "Invitados en la campaña de GP",
+            "Invitados en la campaña de iglesia",
             ""
           ]
             const total_weeks = [
@@ -31,44 +23,11 @@
             "Tercera semana",
             "Cuarta semana"
           ]
-          
-            const initialData = {
-            weeks: [{
-              concept1: 0,
-              concept2: 0,
-              concept3: 0,
-              concept4: 0,
-              concept5: 0,
-              concept6: 0,
-              concept7: 0,
-              concept8: 0,
-              concept9: 0,
-              concept10: 0
-            }]
-          }
 
-            const schema = yup.object().shape({
-            weeks: yup.array().of(
-              yup.object().shape({
-                concept1:yup.number(),
-                concept2:yup.number(),
-                concept3:yup.number(),
-                concept4:yup.number(),
-                concept5:yup.number(),
-                concept6:yup.number(),
-                concept7:yup.number(),
-                concept8:yup.number(),
-                concept9:yup.number(),
-                concept10:yup.number()
-              })
-            ).strict(),
-          })
-            return {
+          return {
                 concepts,
                 total_weeks,
-                initialData,
-                schema
-            }
+          }
         }
     }
 </script>
@@ -95,55 +54,46 @@
               </tr>
             </thead>
             <tbody>
-              <FieldArray name="weeks" v-slot="{ fields }">
-                <tr v-for="(field, index) in fields" :key="field.key">
-                    <th v-text="total_weeks[index]"></th>
-                    <td>
-                      <Field :name="`weeks[${index}].concept1`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept1`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                      <Field :name="`weeks[${index}].concept2`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept2`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                      <Field :name="`weeks[${index}].concept3`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept3`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                      <Field :name="`weeks[${index}].concept4`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept4`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                      <Field :name="`weeks[${index}].concept5`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept5`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                      <Field :name="`weeks[${index}].concept6`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept6`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                      <Field :name="`weeks[${index}].concept7`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept7`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                      <Field :name="`weeks[${index}].concept8`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept8`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                      <Field :name="`weeks[${index}].concept9`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept9`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                      <Field :name="`weeks[${index}].concept10`" type="number" class="input is-family-monospace has-text-centered"/>
-                      <ErrorMessage :name="`weeks[${index}].concept10`" :class="{'tag is-warning': true }"/>
-                    </td>
-                    <td>
-                    </td>
+                <tr>
+                    <th><strong>Primera semana</strong></th>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
                 </tr>
-              </FieldArray>
             </tbody>
           </table>
       </Form>
+    </div>
+    <div class="columns">
+      <div class="column">
+          <article class="panel is-link">
+              <p class="panel-heading">Reglas de negocio</p>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                Visualizar lo capturado del mes correspondiente.
+              </a>
+          </article>
+      </div>
+      <div class="column">
+          <article class="panel is-link">
+              <p class="panel-heading">Funcionalidad</p>
+              <a class="panel-block">
+                <span class="panel-icon">
+                  <i class="fas fa-book" aria-hidden="true"></i>
+                </span>
+                Obtener la información de la interseccion de iglesias y conceptos.
+              </a>
+          </article>
+      </div>
     </div>
 </template>
