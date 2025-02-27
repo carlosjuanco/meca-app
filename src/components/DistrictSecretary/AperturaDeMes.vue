f3ee73a30a74d178a9ec8b66a674c5f81d7a25f1
carlosjuanco <carlosjuancho328@gmail.com>
2025-02-27 00:11:16 -0600

    SDA-23 Reglas de negocio del componente apertura de mes

--- src/components/DistrictSecretary/AperturaDeMes.vue
+++ src/components/DistrictSecretary/AperturaDeMes.vue
@@ -71,4 +71,59 @@ export default {
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Diciembre</button>
     </div>
   </div>
+  <div class="columns">
+    <div class="column is-full">
+        <article class="panel is-link">
+            <p class="panel-heading">Reglas de negocio</p>
+            <a class="panel-block">
+              <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+              </span>
+              En la lista desplegable, se mostrará únicamente el año actual.
+            </a>
+            <a class="panel-block">
+              <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+              </span>
+              Al iniciar el año estará abierto el mes de Enero y el sistema lo cerrará al iniciar el mes siguiente.
+            </a>
+            <a class="panel-block">
+              <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+              </span>
+              Al iniciar un nuevo mes, el mes anterior quedará cerrado por el sistema y deshabilitado.
+            </a>
+            <a class="panel-block">
+              <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+              </span>
+              Solo el mes actual estará habilitado y abierto, con la posibilidad de cerrar el mes.
+            </a>
+            <a class="panel-block">
+              <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+              </span>
+              Por lo tanto solo en un mes, estará la opción de abierto o cerrado.
+            </a>
+            <a class="panel-block">
+              <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+              </span>
+              Si necesita abrir un mes anterior, tine que llamar a los "Creadores del sistema".
+            </a>
+            <a class="panel-block">
+              <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+              </span>
+              El mes actual o el mes que está aperturado se visualizará en color azul.
+            </a>
+            <a class="panel-block">
+              <span class="panel-icon">
+                <i class="fas fa-book" aria-hidden="true"></i>
+              </span>
+              Por predeterminado estará seleccionado abierto el elemento tipo radio.
+            </a>
+          </article>
+    </div>
+  </div>
 </template>
