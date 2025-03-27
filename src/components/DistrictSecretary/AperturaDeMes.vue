537b030d95fa389444db58122916e67eb70a43ba
carlosjuanco <carlosjuancho328@gmail.com>
2025-03-26 19:20:35 -0600

    SDA-22 Hacer la maqueta del componente apertura de mes

--- src/components/DistrictSecretary/AperturaDeMes.vue
+++ src/components/DistrictSecretary/AperturaDeMes.vue
@@ -35,40 +35,194 @@ export default {
     </div>
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Febrero</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Marzo</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Abril</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
   </div>
   <div class="columns">
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Mayo</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Junio</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Julio</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Agosto</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
   </div>
   <div class="columns">
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Septiembre</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Octubre</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Noviembre</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
     <div class="column">
       <button class="button is-large pt-6 pb-6 is-fullwidth" disabled>Diciembre</button>
+      <div class="columns is-mobile">
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Abierto
+          </label>
+        </div>
+        <div class="column">
+          <label class="radio" disabled>
+            <input type="radio" name="open_or_close" disabled/>
+            Cerrado
+          </label>
+        </div>
+      </div>
     </div>
   </div>
 </template>
