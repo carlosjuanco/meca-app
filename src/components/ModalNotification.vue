d081ae64d46aba7ba9f758ef4895512a82047fb8
carlosjuanco <carlosjuancho328@gmail.com>
2025-05-01 14:36:03 -0600

    Componente modal-notificacion

--- src/components/ModalNotification.vue
+++ src/components/ModalNotification.vue
@@ -26,30 +26,22 @@ export default defineComponent({
 </script>
 <template>
     <div :class="{'modal modal-fx-fadeInScale': true, 'is-active': show}">
-        <div class="modal-background" @click="animation_modal_content = false" v-if="loading == false"></div>
+        <div class="" @click="animation_modal_content = false" v-if="loading == false"></div>
 
-        <div :class="{'modal-content animate__animated': true, 'animate__flipInX': animation_modal_content, 'animate__flipOutX': animation_modal_content == false }"
+        <div :class="{'animate__animated': true, 'animate__bounceOut': animation_modal_content == false }"
         @animationend="animationEndModalContent">
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title has-text-warning has-text-centered" v-text="data.title"></p>
-                    <button class="delete" aria-label="close" @click="animation_modal_content = false"></button>
-                </header>
-                <section class="modal-card-body">
+            <div class="">
+                <section class="" @click="animation_modal_content = false">
                     <div class="columns mb-0">
                       <div class="column is-half is-offset-one-quarter">
                             <figure class="image">
-                                <img class="ml-4 animate__animated animate__flash animate__infinite" src="../assets/advertencia.png" />
+                                <img class="ml-4 animate__animated animate__flash animate__infinite" 
+                                    src="../assets/advertencia.png" />
                                 </figure>
                       </div>
                     </div>
                     <h4 v-for="item in data.message" :key="item" class="subtitle is-4 has-text-centered mb-3" v-text="item"></h4>
-                    <p class="mt-6 mb-6 has-text-white">M</p>
-                    <p class="mt-6 mb-6 has-text-white">M</p>
-                    <p class="mt-6 mb-6 has-text-white">M</p>
                 </section>
-                <footer class="modal-card-foot"> 
-                </footer>
             </div>
         </div>
     </div>
