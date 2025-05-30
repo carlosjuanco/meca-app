7748a116ecbd28f99232ecb557b67fe44cf4bf88
carlosjuanco <carlosjuancho328@gmail.com>
2025-05-30 14:03:31 -0600

    Estilo de escritura CamelCase

--- src/store/index.ts
+++ src/store/index.ts
@@ -16,7 +16,7 @@ type Pages = {
 }
 
 type State = {
-    api_token: string;
+    apiToken: string;
     user: User;
     pages: Pages;
     monthOpen: string;
@@ -24,7 +24,7 @@ type State = {
 
 // Define the initial state
 const state: State = {
-    api_token: localStorage.getItem('api_token') || '',
+    apiToken: localStorage.getItem('apiToken') || '',
     user: {id: 0, name: '', email:''},
     pages: {id: 0, name: '', name_component: ''},
     monthOpen: '',
@@ -33,8 +33,8 @@ const state: State = {
 export default createStore({
     state,
     mutations: {
-        SET_API_TOKEN (state, api_token) {
-            state.api_token = api_token
+        SET_API_TOKEN (state, apiToken) {
+            state.apiToken = apiToken
         },
         SET_USER (state, user) {
             state.user = user
@@ -49,13 +49,13 @@ export default createStore({
     actions: {
         async login ({ commit }, data:Data):Promise<void> {
             const response = await axios.post('/login', data)
-            const api_token = response.data.api_token
+            const apiToken = response.data.apiToken
 
-            localStorage.setItem('api_token', api_token)
+            localStorage.setItem('apiToken', apiToken)
 
-            axios.defaults.headers.common['Authorization'] = `Bearer ${api_token}`
+            axios.defaults.headers.common['Authorization'] = `Bearer ${apiToken}`
 
-            commit('SET_API_TOKEN', api_token)
+            commit('SET_API_TOKEN', apiToken)
             commit('SET_USER', response.data.user)
             commit('SET_PAGES', response.data.user.role.pages)
             commit('SET_MONTHOPEN', response.data.user.monthOpen)
@@ -73,7 +73,7 @@ export default createStore({
             dispatch('destroySession')
         },
         destroySession ({ commit }) {
-            localStorage.removeItem('api_token')
+            localStorage.removeItem('apiToken')
 
             delete axios.defaults.headers.common['Authorization']
 
@@ -85,7 +85,7 @@ export default createStore({
     },
     modules: {},
     getters: {
-        api_token: state => state.api_token,
+        apiToken: state => state.apiToken,
         pages: state => state.pages,
         user: state => state.user,
         monthOpen: state => state.monthOpen,
