b9e9d23b3c6f4e553b59686530cd59fd318a3b84
carlosjuanco <carlosjuancho328@gmail.com>
2026-03-18 19:21:56 -0600

    SS0-10 Hacer la maqueta del componente comunidades

--- src/components/Administrative/AppComunidad.vue
+++ src/components/Administrative/AppComunidad.vue
@@ -77,7 +77,7 @@ export default defineComponent({
         <tr>
           <td class="has-text-left is-vcentered">Santa María Peñoles</td>
           <td class="is-vcentered">
-            <button type="button" class="button is-link">
+            <button type="button" class="button is-link" @click="viewForm()">
               <span class="icon">
                 <i class="fas fa-edit"></i>
               </span>
@@ -90,6 +90,44 @@ export default defineComponent({
           </td>
         </tr>
       </tbody>
+      <tfoot>
+        <tr class="has-background-white-bis">
+          <td colspan="2">
+            <nav class="pagination" role="navigation" aria-label="pagination">
+              <a href="#" class="pagination-previous">Antes</a>
+              <a href="#" class="pagination-next">Siguiente</a>
+              <ul class="pagination-list">
+                <li>
+                  <a href="#" class="pagination-link" aria-label="Goto page 1">1</a>
+                </li>
+                <li>
+                  <span class="pagination-ellipsis">&hellip;</span>
+                </li>
+                <li>
+                  <a href="#" class="pagination-link" aria-label="Goto page 45">45</a>
+                </li>
+                <li>
+                  <a
+                    class="pagination-link is-current"
+                    aria-label="Page 46"
+                    aria-current="page"
+                    >46</a
+                  >
+                </li>
+                <li>
+                  <a href="#" class="pagination-link" aria-label="Goto page 47">47</a>
+                </li>
+                <li>
+                  <span class="pagination-ellipsis">&hellip;</span>
+                </li>
+                <li>
+                  <a href="#" class="pagination-link" aria-label="Goto page 86">86</a>
+                </li>
+              </ul>
+            </nav>
+          </td>
+        </tr>
+      </tfoot>
     </table>
   </div>
   <comunidad-form
