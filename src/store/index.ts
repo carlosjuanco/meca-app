0c37980c588e48d40e1c1d2da5cec361be661c33
carlosjuanco <carlosjuancho328@gmail.com>
2026-05-09 22:28:29 -0600

    SS0-29 Modal eliminar

--- src/store/index.ts
+++ src/store/index.ts
@@ -15,18 +15,10 @@ type Pages = {
     name_component: string;
 }
 
-type DataFromTheEliminationModel = {
-    id: number;
-    description: string;
-    showModalDelete: boolean;
-    acceptDelete: boolean;
-}
-
 type State = {
     apiToken: string;
     user: User;
     pages: Pages;
-    dataFromTheEliminationModel: DataFromTheEliminationModel;
 }
 
 // Define the initial state
@@ -39,8 +31,6 @@ const state: State = {
     apiToken,
     user: {id: 0, name: '', email:''},
     pages: {id: 0, name: '', name_component: ''},
-    dataFromTheEliminationModel: { id: 0, description: '', 
-        showModalDelete: false, acceptDelete: false }
 };
 
 export default createStore({
@@ -55,9 +45,6 @@ export default createStore({
         },
         SET_PAGES (state, pages) {
             state.pages = pages
-        },
-        SET_SHOW_MODAL_DELETE (state, data) {
-            state.dataFromTheEliminationModel = data
         },
     },
     actions: {
@@ -93,15 +80,11 @@ export default createStore({
             commit('SET_PAGES', [])
             commit('SET_USER', {})
         },
-        modalDelete({ commit }, data:DataFromTheEliminationModel) {
-            commit('SET_SHOW_MODAL_DELETE', data)
-        },
     },
     modules: {},
     getters: {
         apiToken: state => state.apiToken,
         pages: state => state.pages,
         user: state => state.user,
-        dataFromTheEliminationModel: state => state.dataFromTheEliminationModel,
     }
 })
