5dfccf23e6e0c642796d663e35c93cdcd052fdb9
carlosjuanco <carlosjuancho328@gmail.com>
2026-08-28 11:30:32 -0600

    SS0-27 Validar las reglas de negocio del componente comunidades

--- src/components/Administrative/ComunidadForm.vue
+++ src/components/Administrative/ComunidadForm.vue
@@ -156,7 +156,7 @@ export default defineComponent ({
                   @blur="handleBlur"
                   ref="firstInput"
                   type="text"
-                  :class="{ 'is-danger': false, 'input': true }"
+                  class="input"
                   placeholder="Comunidad"
                 />
               </Field>
